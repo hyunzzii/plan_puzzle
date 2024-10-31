4581079b3c8446788a3f8e03c5326d7379b6d678
hyunzzii <yhynym@gmail.com>
2024-11-01 03:32:19 +0900

    Init: 프로젝트 시작

---
+++ src/main/java/com/sloth/plan_puzzle/PlanPuzzleApplication.java
@@ -0,0 +1,13 @@
+package com.sloth.plan_puzzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanPuzzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanPuzzleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sloth/plan_puzzle/PlanPuzzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sloth.plan_puzzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanPuzzleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
